101acd76321ecef1677f39fb5c00c6c60f280b69
Jaylen <jnixo0033@launchpadphilly.org>
2024-11-14 23:53:46 -0500

    Add files via upload

--- ecom.sql
+++ ecom.sql
@@ -1,11 +1,15 @@
 CREATE DATABASE ecommerce;
 
 CREATE TABLE customers(
-	unique_id INT AUTO_INCREMENT PRIMARY KEY,
+	user_id INT AUTO_INCREMENT PRIMARY KEY,
     first_name VARCHAR(20) NOT NULL,
     last_name VARCHAR(50) NOT NULL,
-    email VARCHAR(50),
+    email VARCHAR(50) UNIQUE,
     phone_number VARCHAR(20),
     address VARCHAR(20),
     state VARCHAR(20)
+);
+
+CREATE TABLE products(
+	product_id INT AUTO_INCREMENT PRIMARY KEY
 );
